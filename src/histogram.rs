7bc21cb4e9ec5cb4f5453ce3dc007fc1914a6fd4
Ruoyi Wang <endvroy@gmail.com>
2021-04-18 14:23:26 -0400

    new parallel histogram impl with reduce op

--- src/histogram.rs
+++ src/histogram.rs
@@ -16,18 +16,20 @@ pub mod single_threaded {
 pub mod parallel {
     use rayon::prelude::*;
 
+    fn combine_histograms(x: Vec<usize>, y: Vec<usize>) -> Vec<usize> {
+        let mut result = x;
+        for i in 0..y.len() {
+            result[i] += y[i];
+        }
+        result
+    }
 
     fn calc_histogram(data: &[usize], n_bins: usize, n_threads: usize) -> Vec<usize> {
-        let mut histogram = vec![0; n_bins];
         let chunk_size = data.len() / n_threads;
-        let local_histograms:Vec<_> = data.chunks(chunk_size).par_bridge()
+        let result = data.chunks(chunk_size).par_bridge()
             .map(|x| super::single_threaded::calc_histogram(x, n_bins))
-            .collect();
-        for local_histogram in local_histograms {
-            for i in 0..histogram.len() {
-                histogram[i] += local_histogram[i];
-            }
-        }
-        histogram
+            .reduce_with(|x, y| combine_histograms(x, y))
+            .unwrap();
+        result
     }
 }
