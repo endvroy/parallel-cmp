4960ea67e99d43629ab78ede2aef185e92c64e31
Ruoyi Wang <endvroy@gmail.com>
2021-04-19 19:33:13 -0700

    now also print out the result

--- src/bin/histogram.rs
+++ src/bin/histogram.rs
@@ -13,12 +13,14 @@ fn main() {
     let data = data_gen::histogram_data::read(path);
     match args[2].as_str() {
         "serial" => {
-            histogram::single_threaded::calc_histogram(&data, 20);
+            let result = histogram::single_threaded::calc_histogram(&data, 20);
+            println!("{:?}", result);
         }
         "parallel" => {
             let n_threads = usize::from_str(args[3].as_str()).unwrap();
             rayon::ThreadPoolBuilder::new().num_threads(n_threads).build_global().unwrap();
-            histogram::parallel::calc_histogram(&data, 20, n_threads);
+            let result = histogram::parallel::calc_histogram(&data, 20, n_threads);
+            println!("{:?}", result);
         }
         _ => {
             panic!("arg must be serial or parallel");
